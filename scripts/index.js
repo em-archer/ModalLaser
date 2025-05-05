ad875bea56f602397b9e839b88c5f517c102798f
Emily <emily.archer@desy.de>
2025-05-05 19:59:32 +0200

    Updating html and js

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 // elements
-const crossSelect = document.getElementById('cross-select')
-const realSelect = document.getElementById('3d-real-select')
-const complexSelect = document.getElementById('3d-complex-select')
+const hermiteSelect = document.getElementById('HG-select')
+const laguerreSelect = document.getElementById('LG-select')
 const graphEl = document.getElementById('image')
 const selectTable = document.getElementById('select-table')
 
@@ -9,70 +8,51 @@ const selectTable = document.getElementById('select-table')
 const urlInfo = window.location.search
 
 // settings
-let n = 1 
-let l = 0
 let m = 0
-let mode = 'cross'
+let n = 0
+let mode = 'HG'
 
-// ? on end of url in form of ?mode_n_l_m
+// ? on end of url in form of ?mode_m_n
 if (urlInfo && urlInfo.length > 1) {
   const params = urlInfo.substring(1).split('_')
-  let nTemp = parseInt(params[1])
-  let lTemp = parseInt(params[2])
-  let mTemp = parseInt(params[3])
+  let mTemp = parseInt(params[1])
+  let nTemp = parseInt(params[2])
   let modeTemp = params[0]
   
   if (
-    (0 < nTemp && nTemp < 8)
-    && (0 <= lTemp && lTemp < nTemp)
-    && (-lTemp - 1 < mTemp && mTemp < lTemp + 1 )
-    && (mode === 'cross' || mode === '3d-real' || mode === '3d-complex')
+    (0 < nTemp && nTemp < 5)
+    && (0 <= mTemp && mTemp < 5)
+    && (mode === 'HG' || mode === 'LG')
   ) {
     mode = modeTemp
-    n = nTemp
-    l = lTemp
     m = mTemp
+    n = nTemp
   }
 }
 
 // set image
-const path = `img/${mode}/${n}_${l}_${m}.png`
+const path = `img/${mode}/${m}_${n}.png`
 graphEl.src = path
 
 // mark selected mode
 document.getElementById(`${mode}-select`).id = 'selected-mode'
 
 // set up links for mode selection
-crossSelect.href = `./?cross_${n}_${l}_${m}`
-realSelect.href = `./?3d-real_${n}_${l}_${m}`
-complexSelect.href = `./?3d-complex_${n}_${l}_${m}`
-
-// subshell numbers to letters table
-subshellTable = {
-  0: 's',
-  1: 'p',
-  2: 'd',
-  3: 'f',
-  4: 'g',
-  5: 'h',
-  6: 'i'
-}
+hermiteSelect.href = `./?HG_${m}_${n}`
+laguerreSelect.href = `./?LG_${m}_${n}`
 
 // create table for orbital selection
-for (let nTable = 1; nTable < 8; nTable += 1) {
-  let tableRow = `<div style="background-color:hsl(${nTable * 50}, 100%, 80%)">`
+for (let nTable = 1; nTable < 5; nTable += 1) {
 
-  for (let lTable = 0; lTable < nTable; lTable += 1) {
+  for (let mTable = 1; mTable < 5; mTable += 1) {
     let subshellSection = `<div class="subshell-container"><div class="links-container">`
 
-    for (let mTable = -lTable; mTable <= lTable; mTable += 1) {
       subshellSection += `<a 
-        href="./?${mode}_${nTable}_${lTable}_${mTable}" 
-        class="orbital" id="${nTable === n && lTable === l && mTable === m ? "selected-orbital" : ""}">${mTable}
+        href="./?${mode}_${mTable}_${nTable}" 
+        class="modal" id="${mTable === m && nTable === n ? "selected-orbital" : ""}">${mTable}
       </a>`
-    }
 
-    subshellSection += `</div><div class='labels-container'>${nTable}${subshellTable[lTable]}</div></div>`
+    subshellSection += `</div><div class='labels-container'>${mTable}${nTable}</div></div>`
     tableRow += subshellSection
   }
 
